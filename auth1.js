a6d9395189581ab5a8522f62c91aa9d1932636b5
SuetLi1014 <suetli.lam.2020@scis.smu.edu.sg>
2022-03-25 01:18:22 +0800

    add login function, half done google sign in

--- auth1.js
+++ auth1.js
@@ -4,7 +4,6 @@ auth.onAuthStateChanged(user => {
 
 const signupForm = document.querySelector("#signUpForm");
 
-
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -52,51 +51,81 @@ signupForm.addEventListener('submit', (e) => {
 
 })
 
-// google sign in still doesnt work, need time to figure out later
-function googleSignIn() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
+//login
+const loginForm = document.querySelector("#loginForm");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then((result) => {
-    if (result.credential) {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
+    const email = loginForm['loginEmail'].value;
+    const password = loginForm['loginPswd'].value;
 
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+    console.log(email, password)
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user)
+        loginForm.reset()
+
+        swal("Done!", "You have successfully login!", "success").then(function () {
+            window.location = 'index.html';
+        }
+        )
+
+    }).catch(error => swal("Oh no!", error.message, "error"))
+
+
+})
+
+function showLogin() {
+    console.log('login btn clicked')
+    var loginpage = document.getElementById('login')
+    var signuppage = document.getElementById('register')
+
+    signuppage.style.display = 'none'
+    login.style.display = 'block'
 
 }
+function loginWithGoogle(event) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            // /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+            console.log('google', credential)
+            var token = credential.accessToken;
+           
+            var user = result.user;
+            // console.log(user.email, user.display_name)
+            console.log('google sign in', user)
+            window.location = 'index.html';
 
+            // auth.createUserWithEmailAndPassword(email, password).then(cred => {
+
+            //     console.log(cred.user)
+
+            //     db.collection('users').doc(cred.user.uid).set({
+            //         user_name: signupForm['signUpUsername'].value,
+            //         user_email: signupForm['signUpEmail'].value,
+            //         residential_address: signupForm['signUpAddress'].value,
+            //         phone: signupForm['signUpPhone'].value
+
+            //     });
+            // })
+            //     .catch(error => console.log(error.message))
+
+
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log(error)
+        });
+
+}
 firebase.auth().signOut().then(function () {
     // Sign-out successful.
 }).catch(function (error) {
